6fe354a28bae00855f6e25126944ca163fb41854
Uwe Krueger <uwe.krueger@sap.com>
2021-02-03 12:59:45 +0100

    test for streamwatcher stop race condition

--- staging/src/k8s.io/apimachinery/pkg/watch/streamwatcher_test.go
+++ staging/src/k8s.io/apimachinery/pkg/watch/streamwatcher_test.go
@@ -20,32 +20,66 @@ import (
 	"fmt"
 	"io"
 	"reflect"
+	goruntime "runtime"
+	"sync"
 	"testing"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	. "k8s.io/apimachinery/pkg/watch"
 )
 
 type fakeDecoder struct {
-	items chan Event
-	err   error
+	lock   sync.Mutex
+	items  chan Event
+	err    error
+	count  int
+	closed int
 }
 
-func (f fakeDecoder) Decode() (action EventType, object runtime.Object, err error) {
-	if f.err != nil {
-		return "", nil, f.err
+func (f *fakeDecoder) getErr() error {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+	return f.err
+}
+
+func (f *fakeDecoder) setErr(err error) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+	f.err = err
+}
+
+func (f *fakeDecoder) getCount() int {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+	return f.count
+}
+
+func (f *fakeDecoder) Decode() (action EventType, object runtime.Object, err error) {
+	err = f.getErr()
+	if err != nil {
+		f.lock.Lock()
+		defer f.lock.Unlock()
+		f.count++
+		return "", nil, err
 	}
 	item, open := <-f.items
 	if !open {
 		return action, nil, io.EOF
 	}
+	f.lock.Lock()
+	defer f.lock.Unlock()
+	f.count++
 	return item.Type, item.Object, nil
 }
 
-func (f fakeDecoder) Close() {
-	if f.items != nil {
+func (f *fakeDecoder) Close() {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+	if f.items != nil && f.closed == 0 {
 		close(f.items)
 	}
+	f.closed++
 }
 
 type fakeReporter struct {
@@ -62,7 +96,7 @@ func TestStreamWatcher(t *testing.T) {
 		{Type: Added, Object: testType("foo")},
 	}
 
-	fd := fakeDecoder{items: make(chan Event, 5)}
+	fd := &fakeDecoder{items: make(chan Event, 5)}
 	sw := NewStreamWatcher(fd, nil)
 
 	for _, item := range table {
@@ -84,7 +118,7 @@ func TestStreamWatcher(t *testing.T) {
 }
 
 func TestStreamWatcherError(t *testing.T) {
-	fd := fakeDecoder{err: fmt.Errorf("test error")}
+	fd := &fakeDecoder{err: fmt.Errorf("test error")}
 	fr := &fakeReporter{}
 	sw := NewStreamWatcher(fd, fr)
 	evt, ok := <-sw.ResultChan()
@@ -103,5 +137,79 @@ func TestStreamWatcherError(t *testing.T) {
 	_, ok = <-sw.ResultChan()
 	if ok {
 		t.Fatalf("unexpected open channel")
+	}
+}
+
+func TestStreamWatcherStopRace(t *testing.T) {
+	table := []Event{
+		{Type: Added, Object: testType("foo")},
+		{Type: Added, Object: testType("bar")},
+	}
+
+	fd := &fakeDecoder{items: make(chan Event, 5)}
+	fr := &fakeReporter{}
+	sw := NewStreamWatcher(fd, fr)
+
+	for i, item := range table {
+		fd.items <- item
+		fmt.Printf("sent event %d: %+v\n", i+1, item)
+	}
+
+	fmt.Printf("waiting for event 1\n")
+	got, open := <-sw.ResultChan()
+	if !open {
+		t.Errorf("unexpected early close")
+	}
+	if e, a := table[0], got; !reflect.DeepEqual(e, a) {
+		t.Errorf("expected %v, got %v", e, a)
+	} else {
+		fmt.Printf("GOT 1: %+v\n", got)
+	}
+
+	for fd.getCount() < 2 {
+		fmt.Printf("waiting for receive...\n")
+		time.Sleep(time.Second)
+	}
+	fd.setErr(fmt.Errorf("some stop error on underlying watch stream"))
+	fd.Close()
+	// now next decode would fail, it is called after the next successful consumption
+
+	fmt.Printf("waiting for event 2\n")
+	got, open = <-sw.ResultChan()
+	if !open {
+		t.Errorf("unexpected early close")
+	}
+	if e, a := table[1], got; !reflect.DeepEqual(e, a) {
+		t.Errorf("expected %v, got %v", e, a)
+	} else {
+		fmt.Printf("GOT 2: %+v\n", got)
+	}
+
+	// ok, now the streamwatcher receive would try to decode the next
+	// element from the stream, which will provide the error set above.
+	// wait, until the error has been consumed, this will initiate
+	// the error send request.
+	for fd.getCount() < 3 {
+		fmt.Printf("waiting for error received...\n")
+		time.Sleep(time.Second)
+	}
+	sw.Stop()
+	// Be sure the receive go routine had a chance to run.
+	// (and react on the new stop channel)
+	goruntime.Gosched()
+	// now the send operation should have been canceled
+	// (and the go routine should have been finished)
+	got, open = <-sw.ResultChan()
+	// in real life scenario this receive would not happen anymore after
+	// the stream has been externally stopped, so the sending go routine
+	// would block forever
+	if open {
+		fmt.Printf("GOT: %+v\n", got)
+		t.Errorf("unexpected send")
+	} else {
+		goruntime.Gosched()
+		if fd.closed != 2 {
+			t.Errorf("receive go routine not stopped yet")
+		}
 	}
 }
